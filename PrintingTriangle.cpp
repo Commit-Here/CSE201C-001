d63d0bbd33180f1e9cef3af712a37c605e785c45
Commit-Here <burhanqibal86@gmail.com>
2022-01-22 13:01:21 +0530

    Adding a program which  Prints a Right Angled Triangle , with height as 20 (X) and Base as 20.

---
+++ PrintingTriangle.cpp
@@ -0,0 +1,19 @@
+/*
+    Printing a Right Angled Triangle , with height as 20 (X) and Base as 20.
+*/
+#include<iostream>
+
+using namespace std;
+int main() {
+    int rows = 0, cols = 0;
+    // Print Until both are  20.
+    while (rows != 20 && cols != 20) {
+        rows++, cols = 0;
+        // print until both are equal.
+        while(rows != cols) {
+            cout<<"☮︎";
+            cols++;
+        } // while
+        cout<<endl;
+    } // while 
+} // main
